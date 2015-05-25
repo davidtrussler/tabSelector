fb2e5975c7a89ef48141bffc6bb44cf7804714ad
David Trussler <mail@davidtrussler.net>
2015-05-25 22:08:21 +0100

    initial tabs layout
    Set widths of tabs dynamically to fill width

--- js/tabsView.js
+++ js/tabsView.js
@@ -3,5 +3,23 @@ var app = app || {};
 app.TabsView = function() {
 	this.setUpTabs = function() {
 		console.log('setUpTabs!'); 
+
+		// set widths of tabs according to nuumber
+		var uls = document.getElementsByTagName('ul'); 
+		var tabs; 
+
+		for (var i = 0; i < uls.length; i++) {
+			if (uls[i].className.indexOf('tabs') != -1) {
+				tabs = uls[i].children; 
+			}
+
+			break; 
+		};  
+
+		for (var i = 0; i < tabs.length; i++) {
+			var tab = tabs[i];
+
+			tab.style.width = 100 / tabs.length + '%'; 
+		}; 
 	}
 }
